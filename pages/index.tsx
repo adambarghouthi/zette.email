9afdd2ebceeb065dd8e0cf400b8c18dca5e798e5
Adam Albarghouthi <adambargh@gmail.com>
2022-04-04 19:26:13 -0400

    minor

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react"
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import Textfield from '../components/Textfield'
 import { sendEmail } from '../lib/apiRequests'
 
